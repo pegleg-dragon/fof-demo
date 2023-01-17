e2210cbdb846d7696a86b15ce42cf73032298546
JK <jeff@hrdag.org>
2023-01-17 13:41:27 -0800

    bugfix: only mix colors when there is more than one color to mix

--- color.js
+++ color.js
@@ -1,3 +1,5 @@
+var seen_outputs = []
+
 function maximally_distant_hue(hues) {
 	// We want an output hue that looks as *different* as possible from all the inputs.
 	// The common case is two inputs (two waves intersecting). in that case, we find
@@ -19,5 +21,15 @@ function maximally_distant_hue(hues) {
 	let average_hue = hues.reduce((a, b) => a + b, 0) / hues.length;
 	average_hue = clamp_to_0_1(average_hue);
 	opposite_average_hue = clamp_to_0_1(average_hue + 0.5);
+
+	if (seen_outputs.indexOf(opposite_average_hue) == -1) {
+		seen_outputs.push(opposite_average_hue);
+		console.log("Input hues: ");
+		for (let i=0; i<hues.length; i++) {
+			console.log(hues[i]);
+		}
+		console.log("output mix: ", opposite_average_hue);
+	}
+
 	return opposite_average_hue;
 }
